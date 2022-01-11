b27d14ccbf7c1b2e36cae8ce61ec0e9b957a5b7a
Meet Bhadeshiya <meet.gajjar2809@gmail.com>
2022-01-11 17:09:32 +0530

    Update our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import HomePage from "./pages/homepage/Homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <main
+    style={{
+      margin: "0 1em",
+      padding: "1em"
+    }}
+  >
+    <h2>HATS PAGE</h2>
+  </main>
+);
+
 function App() {
   return (
-    <div className="">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,20 +1,14 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
-/* REACT COMPONENT USING NORMAL FUNCTION */
-// export default function MenuItem(props) {
-//   return (
-//     <div className="menu-item">
-//       <div className="content">
-//         <h1 className="title">HATS</h1>
-//         <span className="sub-title">SHOP NOW</span>
-//       </div>
-//     </div>
-//   );
-// }
 
-/* REACT COMPONENT USING ARROW FUNCTION */
 const MenuItem = (props) => (
-  <div className={`${props.section.size} menu-item`}>
+  <div
+    className={`${props.section.size} menu-item`}
+    onClick={() =>
+      props.history.push(`${props.match.url}${props.section.linkUrl}`)
+    }
+  >
     <div
       className="background-image"
       style={{
@@ -23,8 +17,8 @@ const MenuItem = (props) => (
     />
     <div className="content">
       <h1 className="title">{props.section.title.toUpperCase()}</h1>
-      <span className="sub-title">SHOP NOW</span>
+      <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
-export default MenuItem;
+export default withRouter(MenuItem);
